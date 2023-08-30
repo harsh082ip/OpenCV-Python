ae7c99a52ddda70ea1e74bf77def113d6546542d
harsh082ip <109286547+harsh082ip@users.noreply.github.com>
2023-08-31 04:07:25 +0530

    Added add_text_to_video.py

---
+++ add_text_to_video.py
@@ -0,0 +1,24 @@
+import cv2
+import datetime
+
+cap = cv2.VideoCapture(0)
+print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret == True:
+
+        font = cv2.FONT_HERSHEY_COMPLEX
+        text = "Width: " + str(cap.get(cv2.CAP_PROP_FRAME_WIDTH)) + 'Height: ' + str(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        datet = str(datetime.datetime.now())
+        frame = cv2.putText(frame, datet, (10,50), font, 1, (0, 0, 255), 2, cv2.LINE_AA)
+        cv2.imshow('frame', frame)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+    else:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
